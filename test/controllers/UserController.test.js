11964f196934e0e6a19f6df8f6c77d9d07b15019
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-08-08 14:50:40 +0100

    Wrote tests for signup on the user controller, tweaked User Model to reflect the user model

--- test/controllers/UserController.test.js
+++ test/controllers/UserController.test.js
@@ -14,32 +14,59 @@ beforeAll(async () => {
 afterAll(() => {
   afterAction();
 });
-
-test('User | create', async () => {
-  const res = await request(api)
-    .post("/public/signup")
-    .set("Accept", /json/)
-    .send({
-      email: "martin@mail.com",
-      password: "securepassword",
-      password2: "securepassword"
-    })
+describe("User | Signup", ()=>{
+  test('Correct User Information', async () => {
+    const res = await request(api)
+      .post("/public/signup")
+      .set("Accept", /json/)
+      .send({
+        name: "Martin",
+        email: "martin@mail.com",
+        phone: "123456",
+        password: "securepassword",
+        password2: "securepassword",
+        user_type: "investor"
+      })
     .expect(200);
 
-  expect(res.body.payload).toBeTruthy();
+    expect(res.body.payload).toBeTruthy();
 
-  const user = await User.findByPk(res.body.payload.id);
+    const user = await User.findByPk(res.body.payload.id);
 
-  expect(user.id).toBe(res.body.payload.id);
-  expect(user.email).toBe(res.body.payload.email);
+    expect(user.id).toBe(res.body.payload.id);
+    expect(user.email).toBe(res.body.payload.email);
 
-  await user.destroy();
-});
+    await user.destroy();
+  });
+
+  test("Wrong User Information", async () => {
+    const res = await request(api)
+      .post("/public/signup")
+      .set("Accept", /json/)
+      .send({
+        name: "Martin",
+        email: "martin@mail.com",
+        phone: "123456",
+        password: "securepassword",
+        password2: "securepassword1",
+        user_type: "investor"
+      })
+
+      expect(res.body.statusCode).toBe(400);
+      expect(res.body.errors.password).toMatch("password does not match");
+      expect(res.body.message).toMatch("Passwords does not match");
+  })
+})
+
 
 test('User | login', async () => {
   const user = await User.create({
-    email: 'martin@mail.com',
-    password: 'securepassword',
+    name: "Martin",
+    email: "martin@mail.com",
+    phone: "123456",
+    password: "securepassword",
+    password2: "securepassword",
+    user_type: "investor"
   });
 
   const res = await request(api)
@@ -60,8 +87,12 @@ test('User | login', async () => {
 
 test('User | get all (auth)', async () => {
   const user = await User.create({
-    email: 'martin@mail.com',
-    password: 'securepassword',
+    name: "Martin",
+    email: "martin@mail.com",
+    phone: "123456",
+    password: "securepassword",
+    password2: "securepassword",
+    user_type: "investor"
   });
 
   const res = await request(api)
