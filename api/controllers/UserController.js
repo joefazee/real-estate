2d838f73cd2e58c9e2437fc4fdebb1465011b967
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-08-08 14:49:19 +0100

    Define signup route, tweaked eslint to support es6 rest

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,112 +6,112 @@ const httpStatus = require("http-status");
 const sendResponse = require("../../helpers/response");
 
 const UserController = () => {
-  const register = async (req, res, next) => {
-    try {
-      const { email, password, password2 } = req.body;
+	const register = async (req, res, next) => {
+		try {
+			const { name, email, phone, password, password2, user_type } = req.body;
 
-      if (password !== password2) {
-        return res.json(
-          sendResponse(
-            httpStatus.BAD_REQUEST,
-            "Passwords does not match",
-            {},
-            { password: "password does not match" }
-          )
-        );
-      }
+			if (password !== password2) {
+				return res.json(
+					sendResponse(
+						httpStatus.BAD_REQUEST,
+						"Passwords does not match",
+						{},
+						{ password: "password does not match" }
+					)
+				);
+			}
 
-      const user = await User.create({
-        email,
-        password
-      });
-      const token = authService().issue({ id: user.id });
+			const user = await User.create({
+				name,
+				email,
+				phone,
+				password,
+				user_type
+			});
+      
+			return res.json(sendResponse(httpStatus.OK, "success", user, null));
+		} catch (err) {
+			next(err);
+		}
+	};
 
-      return res.json(
-        sendResponse(httpStatus.OK, "success", user, null, token)
-      );
-    } catch (err) {
-      next(err);
-    }
-  };
+	const login = async (req, res, next) => {
+		try {
+			const { email, password } = req.body;
 
-  const login = async (req, res, next) => {
-    try {
-      const { email, password } = req.body;
+			const user = await User.findOne({
+				where: {
+					email
+				}
+			});
 
-      const user = await User.findOne({
-        where: {
-          email
-        }
-      });
+			if (!user) {
+				return res.json(
+					sendResponse(
+						httpStatus.NOT_FOUND,
+						"User does not exist",
+						{},
+						{ error: "User does not exist" }
+					)
+				);
+			}
 
-      if (!user) {
-        return res.json(
-          sendResponse(
-            httpStatus.NOT_FOUND,
-            "User does not exist",
-            {},
-            { error: "User does not exist" }
-          )
-        );
-      }
+			if (bcryptService().comparePassword(password, user.password)) {
+				const token = authService().issue({ id: user.id });
 
-      if (bcryptService().comparePassword(password, user.password)) {
-        const token = authService().issue({ id: user.id });
+				return res.json(
+					sendResponse(httpStatus.OK, "success", user, null, token)
+				);
+			}
 
-        return res.json(
-          sendResponse(httpStatus.OK, "success", user, null, token)
-        );
-      }
+			return res.json(
+				sendResponse(
+					httpStatus.BAD_REQUEST,
+					"invalid email or password",
+					{},
+					{ error: "invalid email or password" }
+				)
+			);
+		} catch (err) {
+			next(err);
+		}
+	};
 
-      return res.json(
-        sendResponse(
-          httpStatus.BAD_REQUEST,
-          "invalid email or password",
-          {},
-          { error: "invalid email or password" }
-        )
-      );
-    } catch (err) {
-      next(err);
-    }
-  };
+	const validate = (req, res) => {
+		const { token } = req.body;
 
-  const validate = (req, res) => {
-    const { token } = req.body;
+		authService().verify(token, err => {
+			if (err) {
+				return res.json(
+					sendResponse(
+						httpStatus.UNAUTHORIZED,
+						"Invalid Token!",
+						{},
+						{ error: "Invalid Token!" }
+					)
+				);
+			}
 
-    authService().verify(token, err => {
-      if (err) {
-        return res.json(
-          sendResponse(
-            httpStatus.UNAUTHORIZED,
-            "Invalid Token!",
-            {},
-            { error: "Invalid Token!" }
-          )
-        );
-      }
+			return res.status(200).json({ isvalid: true });
+		});
+	};
 
-      return res.status(200).json({ isvalid: true });
-    });
-  };
+	const getAll = async (req, res) => {
+		try {
+			const users = await User.findAll();
 
-  const getAll = async (req, res) => {
-    try {
-      const users = await User.findAll();
+			return res.json(sendResponse(httpStatus.OK, "success!", users, null));
+		} catch (err) {
+			next(err);
+		}
+	};
 
-      return res.json(sendResponse(httpStatus.OK, "success!", users, null));
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  return {
-    register,
-    login,
-    validate,
-    getAll
-  };
+	return {
+		register,
+		login,
+		validate,
+		getAll
+	};
 };
 
 module.exports = UserController;
