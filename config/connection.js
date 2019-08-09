d71856051c1d95fade984e0069bab8baa8629f59
josemak25 <amakirij@gmail.com>
2019-08-09 12:22:50 +0100

    add fix for env naming

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const development = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT
