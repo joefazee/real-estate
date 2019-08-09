760d3255ec3af1e495bbf3e8f177fbf63266d664
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-09 22:15:56 +0100

    Added expiry column to OTP Sequelize model

--- api/models/OTP.js
+++ api/models/OTP.js
@@ -2,24 +2,28 @@ const Sequelize = require('sequelize');
 const sequelize = require('../../config/database');
 
 const tableName = 'otp';
-
+const FIFTEEN_MINS = 1000 * 60 * 15;
 const OTP = sequelize.define(
   'OTP',
   {
     otp_id: {
-      primaryKey: true,
       type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true
     },
     user_id: {
       type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      allowNull: false
+      allowNull: false,
+      unique: true
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
       unique: true
+    },
+    expiry: {
+      type: Sequelize.DATE,
+      allowNull: false
     }
   },
   { tableName }
