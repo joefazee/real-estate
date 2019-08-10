5caa599702a6f93911b30d9bd972b21a1a2d0fee
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-10 19:32:44 +0100

    Added mail config file

---
+++ config/mail.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const development = {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  username: process.env.MAIL_USERNAME,
+  password: process.env.MAIL_PASSWORD
+};
+
+const testing = {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  username: process.env.MAIL_USERNAME,
+  password: process.env.MAIL_PASSWORD
+};
+
+const production = {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  username: process.env.MAIL_USERNAME,
+  password: process.env.MAIL_PASSWORD
+};
+
+module.exports = {
+  development,
+  testing,
+  production
+};
