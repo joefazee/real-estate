83bde522cb33ebdf6c14d96954700b1a8f482556
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-11 14:58:42 +0100

    add test that ensures user is a seller

--- test/controllers/AgencyProfileController.test.js
+++ test/controllers/AgencyProfileController.test.js
@@ -30,10 +30,10 @@ test('Agency Profile | create (auth)', async () => {
 		password: 'securepassword',
 	};
 
+	// get user details that include id
 	const res = await UserQuery.findByEmail(loginDetails.email);
 
 	const confirmedUser = res.dataValues;
-	delete confirmedUser.password;
 
 	const token = await authService().issue(confirmedUser);
 
@@ -53,5 +53,46 @@ test('Agency Profile | create (auth)', async () => {
 	const agencyProfile = await AgencyProfile.findByPk(res2.body.payload.id);
 	expect(agencyProfile).toBeTruthy;
 	expect(res2.body.payload.user_id).toBe(res.id);
+	await newUser.destroy();
+});
+
+test('Agency Profile | create (user is not a seller)', async () => {
+	const newUser = await User.create({
+		name: 'Martin Luke',
+		email: 'martin@mail.com',
+		password: 'securepassword',
+		password2: 'securepassword',
+		phone: '09057373',
+		user_type: 'investor',
+	});
+
+	const loginDetails = {
+		email: 'martin@mail.com',
+		password: 'securepassword',
+	};
+
+	// get user details that include id
+	const res = await UserQuery.findByEmail(loginDetails.email);
+
+	const confirmedUser = res.dataValues;
+
+	// generate a token
+	const token = await authService().issue(confirmedUser);
+	console.log(token);
+
+	const res2 = await request(api)
+		.post('/private/create_profile')
+		.set('Authorization', `Bearer ${token}`)
+		.set('Content-Type', 'application/json')
+		.send({
+			business_name: 'XYZ Realtors',
+			business_address: '10, Balewa Str',
+			website: 'www.xyzrealtee.com',
+			phone: '123456',
+			email: 'info@xyzrealtee.com',
+		});
+
+	expect(res2.body.statusCode).toBe(401);
+	expect(res2.body.message).toBe('Unauthorized user');
 	await newUser.destroy();
 });
