6cdbed75a75fad32f8d804ddf947ebaa512924e5
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-13 19:15:32 +0100

    Refactored config file for mail

--- config/mail.js
+++ config/mail.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
+let configuration;
 
-const development = {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  username: process.env.MAIL_USERNAME,
-  password: process.env.MAIL_PASSWORD
-};
+switch (process.env.NODE_ENV) {
+  case 'production':
+    configuration = {
+      host: process.env.SEND_GRID_HOST,
+      port: process.env.SEND_GRID_PORT,
+      auth: {
+        user: process.env.SEND_GRID_USERNAME,
+        pass: process.env.SEND_GRID_PASSWORD
+      }
+    };
+    break;
 
-const testing = {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  username: process.env.MAIL_USERNAME,
-  password: process.env.MAIL_PASSWORD
-};
+  default:
+    configuration = {
+      host: process.env.MAIL_TRAP_HOST,
+      port: process.env.MAIL_TRAP_PORT,
+      auth: {
+        user: process.env.MAIL_TRAP_USERNAME,
+        pass: process.env.MAIL_TRAP_PASSWORD
+      }
+    };
+}
 
-const production = {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  username: process.env.MAIL_USERNAME,
-  password: process.env.MAIL_PASSWORD
-};
-
-module.exports = {
-  development,
-  testing,
-  production
-};
+module.exports = configuration;
