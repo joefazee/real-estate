6ef90d6f2543250fb64dd2cc687a9763ea9ed67d
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-14 12:56:40 +0100

    add approvedAt field to seller profile

--- api/models/User.js
+++ api/models/User.js
@@ -1,66 +1,66 @@
-const Sequelize = require("sequelize");
-const bcryptService = require("../services/bcrypt.service");
+const Sequelize = require('sequelize');
+const bcryptService = require('../services/bcrypt.service');
 
-const sequelize = require("../../config/database");
+const sequelize = require('../../config/database');
 
-const AgencyProfile = require("../models/AgencyProfile");
+const AgencyProfile = require('../models/AgencyProfile');
 
 const hooks = {
-  beforeCreate(user) {
-    user.password = bcryptService().hashPassword(user);
-  }
+	beforeCreate(user) {
+		user.password = bcryptService().hashPassword(user);
+	},
 };
 
-const tableName = "users";
+const tableName = 'users';
 
 const User = sequelize.define(
-  "User",
-  {
-    //attributes
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    user_type: {
-      type: Sequelize.ENUM(["admin", "investor", "seller"]),
-      defaultValue: "investor",
-      allowNull: false
-    },
-    email_verified: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: false
-    },
-    id: {
-      type: Sequelize.UUID,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4
-    }
-  },
-  { hooks, tableName }
+	'User',
+	{
+		//attributes
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		phone: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		user_type: {
+			type: Sequelize.ENUM(['admin', 'investor', 'seller']),
+			defaultValue: 'investor',
+			allowNull: false,
+		},
+		email_verified: {
+			type: Sequelize.BOOLEAN,
+			defaultValue: false,
+			allowNull: false,
+		},
+		id: {
+			type: Sequelize.UUID,
+			primaryKey: true,
+			defaultValue: Sequelize.UUIDV4,
+		},
+	},
+	{ hooks, tableName }
 );
 
 User.prototype.toJSON = function() {
-  const values = Object.assign({}, this.get());
+	const values = Object.assign({}, this.get());
 
-  delete values.password;
+	delete values.password;
 
-  return values;
+	return values;
 };
 
-User.hasOne(AgencyProfile, { as: "profile", foreignKey: "user_id" });
+User.hasOne(AgencyProfile, { as: 'profile', foreignKey: 'user_id' });
 
 module.exports = User;
