59bbb2151a3bab0faaaae2c7508d3235dfa813e2
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-15 19:18:44 +0100

    Added tests to check for when OTP is not valid

--- test/controllers/OTPController.test.js
+++ test/controllers/OTPController.test.js
@@ -12,7 +12,10 @@ afterAll(() => {
   afterAction();
 });
 
-test(`OTPController.checkOTP | OTP is valid`, async () => {
+test(`OTPController.checkOTP | OTP is not valid`, async () => {
+  // Test that the old OTP expires when a new OTP is generated
+
+  // Create a test user
   const user = await User.create({
     name: 'Test User',
     email: 'test.user@mail.com',
@@ -22,6 +25,7 @@ test(`OTPController.checkOTP | OTP is valid`, async () => {
     user_type: 'investor'
   });
 
+  // Request password reset and extract OTP from response
   const {
     body: {
       payload: { password }
@@ -31,6 +35,45 @@ test(`OTPController.checkOTP | OTP is valid`, async () => {
     .set('Content-Type', 'application/json')
     .send({ email: 'test.user@mail.com' });
 
+  // Request another password reset to cause the first OTP to expire
+  await request(api)
+    .post(`/public/forgot-password/`)
+    .set('Content-Type', 'application/json')
+    .send({ email: 'test.user@mail.com' });
+
+  // Make GET request to check OTP validity for old OTP which should have expired
+  const response = await request(api).get(`/public/password-reset/${password}`);
+
+  expect(response.body.statusCode).toEqual(404);
+  expect(response.body.message).toBe('otp is not valid');
+
+  await user.destroy();
+});
+
+test(`OTPController.checkOTP | OTP is valid`, async () => {
+  // Test that the OTP is valid before user can reset password
+
+  // Create new user
+  const user = await User.create({
+    name: 'Test User',
+    email: 'test.user@mail.com',
+    password: 'securepassword',
+    password2: 'securepassword',
+    phone: '08023456789',
+    user_type: 'investor'
+  });
+
+  // Request password reset and extract OTP from response
+  const {
+    body: {
+      payload: { password }
+    }
+  } = await request(api)
+    .post(`/public/forgot-password/`)
+    .set('Content-Type', 'application/json')
+    .send({ email: 'test.user@mail.com' });
+
+  // Make GET request to check OTP validity for old OTP which should have expired
   const response = await request(api).get(`/public/password-reset/${password}`);
 
   expect(response.body.statusCode).toEqual(200);
