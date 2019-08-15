50ee87511f7785440ba4db59e285d7ec7011f450
josemak25 <amakirij@gmail.com>
2019-08-15 17:11:19 +0100

    add change to user relationship for user category

--- api/models/UserCategory.js
+++ api/models/UserCategory.js
@@ -16,19 +16,16 @@ const UserCategory = sequelize.define(
     },
     user_id: {
       type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
       allowNull: false
     },
     category_id: {
       type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
       allowNull: false
     }
   },
   { tableName: 'user_categories', timestamps: false, underscored: true }
 );
 
-UserCategory.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
 UserCategory.belongsTo(Category, { as: 'category', foreignKey: 'category_id' });
 
 module.exports = UserCategory;
