f417c38daab6bf6845015cbe14f512266998735a
josemak25 <amakirij@gmail.com>
2019-08-16 10:47:03 +0100

    add verification controller

---
+++ api/controllers/VerificationController.js
@@ -0,0 +1,36 @@
+const httpStatus = require('http-status');
+const sendResponse = require('../../helpers/response');
+const VerificationQuery = require('../queries/verification.queries');
+
+const VerificationController = () => {
+  const verify = async (req, res, next) => {
+    try {
+      const { code } = req.params;
+      const foundCode = await VerificationQuery.findCode(code);
+
+      if (!foundCode) {
+        return res.json(sendResponse(httpStatus.NOT_FOUND, 'Code does not exist', {}, null));
+      }
+
+      const codeIsValid = VerificationQuery.verifyCode(foundCode);
+
+      if (!codeIsValid) {
+        return res.json(
+          sendResponse(httpStatus.UNAUTHORIZED, 'Code has expired', codeIsValid, null)
+        );
+      }
+
+      return res.json(
+        sendResponse(httpStatus.OK, 'Account Verified Successfully!', codeIsValid, null)
+      );
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  return {
+    verify
+  };
+};
+
+module.exports = VerificationController;
