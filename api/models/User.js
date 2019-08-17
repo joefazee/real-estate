de448422f9deb8771e7790386ce592d5cf55a32d
josemak25 <amakirij@gmail.com>
2019-08-17 13:28:42 +0100

    add user signup mailing service on registration

--- api/models/User.js
+++ api/models/User.js
@@ -1,12 +1,39 @@
 const Sequelize = require('sequelize');
-const bcryptService = require('../services/bcrypt.service');
-const AgencyProfile = require('./AgencyProfile');
+const crypto = require('crypto');
 const sequelize = require('../../config/database');
+const { port } = require('../../config');
+const bcryptService = require('../services/bcrypt.service');
+const EmailService = require('../services/email.service');
+const Verification = require('./Verification');
+const AgencyProfile = require('./AgencyProfile');
 const UserCategory = require('./UserCategory');
 
 const hooks = {
-  beforeCreate(user) {
+  async beforeSave(user, { transaction }) {
+    await EmailService.connect();
+
     user.password = bcryptService().hashPassword(user);
+    const code = crypto.randomBytes(5).toString('hex');
+    const payload = { user_id: user.id, code };
+
+    await Verification.create(payload, { transaction });
+
+    const mailTitle = 'Diaspora Invest: Verify Account';
+    const verifyLink = new URL(`http://localhost:${port}/api/v1/public/verify-email/${code}`);
+    const message = `<p>To very your account, please click on the following link: <a href=${verifyLink}>Verify my account</a>.</p>
+      <p>If the link does not work, please copy this URL into your browser and click enter: ${verifyLink}</p>`;
+    const mailBody = `<!DOCTYPE html><html><head><title>Message</title></head><body>${message}</body></html>`;
+
+    EmailService.send({
+      from: process.env.EMAIL_SERVICE_FROM,
+      to: `${user.email}`,
+      subject: `${mailTitle}`,
+      text: 'TEST MAIL',
+      html: `${mailBody}`
+    });
+
+    EmailService.on('success', () => transaction.commit());
+    EmailService.on('error', () => transaction.rollback());
   }
 };
 
@@ -59,6 +86,7 @@ User.prototype.toJSON = function() {
   return values;
 };
 
+User.hasOne(Verification, { as: 'verification', foreignKey: 'user_id' });
 User.hasOne(AgencyProfile, { as: 'profile', foreignKey: 'user_id' });
 User.hasMany(UserCategory, { as: 'user_category', foreignKey: 'user_id' });
 
