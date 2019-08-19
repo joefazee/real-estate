eac8b1b75f72f3f54786d442be59a5d7a37fe042
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-20 00:58:33 +0100

    add property-listing route

--- config/routes/privateRoutes.js
+++ config/routes/privateRoutes.js
@@ -5,52 +5,62 @@ const IsAdmin = require('../../api/middlewares/IsAdmin');
 const IsInvestor = require('../../api/middlewares/IsInvestor');
 const getCategories = require('../../helpers/categories');
 const profileValidation = require('../../api/validations/agencyProfile.valiation');
-const isSelllerMiddleware = require('../../api/middlewares/isSellerMiddleware');
+const IsSeller = require('../../api/middlewares/IsSeller');
 const hasCreatedProfile = require('../../api/middlewares/hasCreatedProfile');
 const uploadFile = require('../../api/middlewares/fileUpload');
+const propertyValidation = require('../../api/validations/propertyListing.validation');
 
 const privateRoutes = {
-  'GET /users': {
-    path: 'UserController.getAll',
-    middlewares: [IsAdmin]
-  },
-  'GET /agency-profiles': {
-    path: 'AgencyProfileController.getAllProfiles',
-    middlewares: [IsAdmin]
-  },
-
-  'POST /create-profile': {
-		path: 'AgencyProfileController.createProfile',
-    middlewares: [validate(profileValidation.createProfile, { abortEarly: false }), isSelllerMiddleware, hasCreatedProfile, 
-      uploadFile('documents')
-    ],
+	'GET /users': {
+		path: 'UserController.getAll',
+		middlewares: [IsAdmin],
+	},
+	'GET /agency-profiles': {
+		path: 'AgencyProfileController.getAllProfiles',
+		middlewares: [IsAdmin],
 	},
 
-  'GET /categories': 'CategoryController.getAll',
+	'POST /create-profile': {
+		path: 'AgencyProfileController.createProfile',
+		middlewares: [
+			validate(profileValidation.createProfile, { abortEarly: false }),
+			isSelllerMiddleware,
+			hasCreatedProfile,
+			uploadFile('documents'),
+		],
+	},
 
-  'GET /user-category/:id': {
-    path: 'UserCategoryController.getAll',
-    middlewares: [validate(UserIdValidation.validateUserId, { abortEarly: false }), IsInvestor]
-  },
+	'GET /categories': 'CategoryController.getAll',
 
-  'POST /category': {
-    path: 'CategoryController.create',
-    middlewares: [validate(paramValidation.createCategory, { abortEarly: false }), IsAdmin]
-  },
+	'GET /user-category/:id': {
+		path: 'UserCategoryController.getAll',
+		middlewares: [validate(UserIdValidation.validateUserId, { abortEarly: false }), IsInvestor],
+	},
 
-  'POST /select-category': {
-    path: 'UserCategoryController.create',
-    middlewares: [
-      validate(paramValidation.createCategory, { abortEarly: false }),
-      IsInvestor,
-      getCategories
-    ]
-  },
+	'POST /category': {
+		path: 'CategoryController.create',
+		middlewares: [validate(paramValidation.createCategory, { abortEarly: false }), IsAdmin],
+	},
 
-  'POST /approve-profile/:id': {
-    path: 'AgencyProfileController.approveProfile',
-    middlewares: [validate(UserIdValidation.validateUserId, { abortEarly: false }), IsAdmin]
-  }
+	'POST /select-category': {
+		path: 'UserCategoryController.create',
+		middlewares: [validate(paramValidation.createCategory, { abortEarly: false }), IsInvestor, getCategories],
+	},
+
+	'POST /approve-profile/:id': {
+		path: 'AgencyProfileController.approveProfile',
+		middlewares: [validate(UserIdValidation.validateUserId, { abortEarly: false }), IsAdmin],
+	},
+
+	'POST /user-category': {
+		path: 'UserCategoryController.create',
+		middlewares: [validate(paramValidation.createCategory, { abortEarly: false }), IsInvestor, getCategories],
+	},
+
+	'POST /property-listing': {
+		path: 'PropertyListingController.createProperty',
+		middlewares: [validate(propertyValidation.createProperty, { abortEarly: false }), IsSeller],
+	},
 };
 
 module.exports = privateRoutes;
