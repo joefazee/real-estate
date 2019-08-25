99546f40cbbfe0f4fcc0759978f642983843cb91
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-26 00:22:42 +0100

    Added images to agency profile model

--- api/models/AgencyProfile.js
+++ api/models/AgencyProfile.js
@@ -5,53 +5,57 @@ const Document = require('./Document');
 const tableName = 'agency_profiles';
 
 const AgencyProfile = sequelize.define(
-	'AgencyProfile',
-	{
-		id: {
-			primaryKey: true,
-			type: Sequelize.UUID,
-			defaultValue: Sequelize.UUIDV4,
-		},
-		user_id: {
-			type: Sequelize.UUID,
-			unique: true,
-		},
-		business_name: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-		business_address: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-		website: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-		phone: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-		email: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-		isApproved: {
-			type: Sequelize.BOOLEAN,
-			defaultValue: false,
-			allowNull: false,
-		},
-		approvedAt: {
-			type: 'TIMESTAMP',
-			allowNull: true,
-		},
-	},
-	{ tableName, timestamps: false }
+  'AgencyProfile',
+  {
+    id: {
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    user_id: {
+      type: Sequelize.UUID,
+      unique: true,
+    },
+    business_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    business_address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    website: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    isApproved: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    approvedAt: {
+      type: 'TIMESTAMP',
+      allowNull: true,
+    },
+    images: {
+      type: Sequelize.TEXT('long'),
+      allowNull: true,
+    },
+  },
+  { tableName, timestamps: false }
 );
 
 AgencyProfile.hasMany(Document, {
-	as: 'profile_document',
-	foreignKey: 'profile_id',
+  as: 'profile_document',
+  foreignKey: 'profile_id',
 });
 
 module.exports = AgencyProfile;
