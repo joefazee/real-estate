998936eef3a663c8cf6a5a8e92c6a9bd6bee8a4e
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-25 13:30:23 +0100

    Added filterBy and findAll queries for agency profile

--- api/queries/agency.profile.queries.js
+++ api/queries/agency.profile.queries.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const sequelize = require('../../config/database');
 const AgencyProfile = require('../models/AgencyProfile');
 
 class AgencyProfileQueries {
@@ -15,8 +16,20 @@ class AgencyProfileQueries {
     return this.Model.findOne({ where: { user_id } });
   }
 
-  findAll() {
-    return this.Model.findAll();
+  findAll(search, { limit, offset }) {
+    // return this.Model.findAll();
+    return sequelize.query(
+      `
+    SELECT * 
+    FROM agency_profiles 
+    JOIN users ON agency_profiles.user_id = users.id 
+    ORDER BY business_name ASC 
+    LIMIT :offset, :limit`,
+      {
+        replacements: { ...search, offset, limit },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
   }
 
   findByProfileId(id) {
@@ -27,6 +40,22 @@ class AgencyProfileQueries {
     return this.Model.update(
       { isApproved: true, approvedAt: Sequelize.literal('CURRENT_TIMESTAMP') },
       { where: { id: profile.id } }
+    );
+  }
+
+  filterBy(search, { limit, offset }) {
+    return sequelize.query(
+      `
+    SELECT * 
+    FROM agency_profiles 
+    JOIN users ON agency_profiles.user_id = users.id 
+    WHERE isApproved = :approved
+    ORDER BY business_name ASC 
+    LIMIT :offset, :limit`,
+      {
+        replacements: { ...search, offset, limit },
+        type: sequelize.QueryTypes.SELECT,
+      }
     );
   }
 }
